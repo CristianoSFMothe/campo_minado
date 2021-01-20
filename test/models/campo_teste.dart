750921dcd4b318021d13ac221849b27dc294538b
cristianoDevSystem <cristianodevsystem@gmail.com>
2021-01-20 20:28:49 -0300

    Criando o widget teste
    Aqui será criando os exemplos dos testes que irão ser realizados

---
+++ test/models/campo_teste.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+
+  group('campo', () {
+
+    test('Abri Campo COM Explosão', () {
+
+    });
+    
+    test('Abri Campo SEM Explosão', () {
+
+    });
+    
+    test('Adicionar NÃO vizinho', () {
+
+    });
+
+    
+    test('Adicionar  vizinho', () {
+
+    });
+    
+  });
+}
