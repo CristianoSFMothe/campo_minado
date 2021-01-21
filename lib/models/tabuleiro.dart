ea6f38317ea5f06f00ac734994be7101d6927e62
cristianoDevSystem <cristianodevsystem@gmail.com>
2021-01-20 22:05:53 -0300

    Metodo de adicionar vizinos

--- lib/models/tabuleiro.dart
+++ lib/models/tabuleiro.dart
@@ -9,6 +9,7 @@ class Tabuleiro {
 
   final List<Campo> _campos = [];
 
+  // Construtor dos atributos
   Tabuleiro({
     @required this.linhas,
     @required this.colunas,
@@ -16,12 +17,23 @@ class Tabuleiro {
 
   }) {
     _criarCampos();
+    _relacionarVizinhos();
   }
 
+  // Metodo para criar os campos
   void _criarCampos() {
     for(int l = 0; l < linhas; l++) {
       for(int c = 0; c < colunas; c++) {
         _campos.add(Campo(linha: l, coluna: c));
+      }
+    }
+  }
+
+  // Metodo para relacionar os vizinhos
+  void _relacionarVizinhos() {
+    for(var campo in _campos) {
+      for(var vizinho in _campos){
+        campo.addVizinho(vizinho);
       }
     }
   }
