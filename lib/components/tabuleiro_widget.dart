bf1a53f6b66700ddebbdf8f937448a3b8eaa6680
cristianoDevSystem <cristianodevsystem@gmail.com>
2021-01-22 20:06:10 -0300

    Widget Tabuleiro
    Campo responsavel pela o gerenciando das imagens no tabuleiro, um widget sem estado, pois as alterações será feita em cada campo propriamente.

---
+++ lib/components/tabuleiro_widget.dart
@@ -0,0 +1,33 @@
+import 'package:campo_minado/components/campo_widget.dart';
+
+import '../models/campo.dart';
+import '../models/tabuleiro.dart';
+import 'package:flutter/material.dart';
+
+class TabuleiroWidget extends StatelessWidget {
+  final Tabuleiro tabuleiro;
+  final void Function(Campo) onAbrir;
+  final void Function(Campo) onAlternarMarcacao;
+
+  TabuleiroWidget({
+    @required this.tabuleiro,
+    @required this.onAbrir,
+    @required this.onAlternarMarcacao,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GridView.count(
+        crossAxisCount: tabuleiro.colunas,
+        children: tabuleiro.campos.map((c) {
+          return CampoWidget(
+            campo: c,
+            onAbrir: onAbrir,
+            onAlternarMaracacao: onAlternarMarcacao,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
